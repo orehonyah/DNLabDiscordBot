5c99ddb460cc08915e12337cc7879d50131a82d0
Jung Jinwook <orehonyah@gmail.com>
2020-02-28 17:11:00 +0900

    Update corona.py : luha_bot 폴더의 greeter.py를 참고하여 형식 변경

--- oreho_bot/corona.py
+++ oreho_bot/corona.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-
 import requests
 from bs4 import BeautifulSoup
 
-# 전체환자
-res = requests.get('http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun=')
-soup = BeautifulSoup(res.text, 'html.parser')
-sickpeople = soup.select('#content > div > div.bv_content > div > div:nth-child(3) > table > tbody > tr:nth-child(1) > td')
-print("전체 확진자 : "+str(sickpeople[0].contents[0]))
+class Corona(object):
 
-# 대전광역시 환자
-res = requests.get('https://www.daejeon.go.kr/corona19/index.do')
-soup = BeautifulSoup(res.text, 'html.parser')
-sickpeople = soup.select('#contBox > div:nth-child(2) > div > ul > li.tab-1 > div.txt > strong')
-print("대전광역시 확진자 : "+str(sickpeople[0].contents[0])+" 명")
-
-
+    @staticmethod
+    def corona(ctx, args=()):
+        speech = '{0.author.mention} \n' + Corona.corona_kr() + '\n'+Corona.corona_daejeon()
+        return speech.format(ctx)
+    
+    # 전국환자
+    @staticmethod
+    def corona_kr():
+        res = requests.get('http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun=')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        sickpeople = soup.select('#content > div > div.bv_content > div > div:nth-child(3) > table > tbody > tr:nth-child(1) > td')
+        return "전체 확진자 : "+str(sickpeople[0].contents[0])
+    
+    # 대전광역시 환자
+    @staticmethod
+    def corona_daejeon():
+        res = requests.get('https://www.daejeon.go.kr/corona19/index.do')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        sickpeople = soup.select('#contBox > div:nth-child(2) > div > ul > li.tab-1 > div.txt > strong')
+        return "대전광역시 확진자 : "+str(sickpeople[0].contents[0])+" 명"
