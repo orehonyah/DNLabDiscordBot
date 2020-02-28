f2803be7c94c34e830e0e9d5c6c9dc3624e557b7
Jung Jinwook <orehonyah@gmail.com>
2020-02-28 16:49:53 +0900

    Update corona.py : 대전광역시 확진자수 추가

--- oreho_bot/corona.py
+++ oreho_bot/corona.py
@@ -3,8 +3,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+# 전체환자
 res = requests.get('http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun=')
 soup = BeautifulSoup(res.text, 'html.parser')
-table = soup.select('#content > div > div.bv_content > div > div:nth-child(3) > table > tbody > tr:nth-child(1) > td')
-print(table)
+sickpeople = soup.select('#content > div > div.bv_content > div > div:nth-child(3) > table > tbody > tr:nth-child(1) > td')
+print("전체 확진자 : "+str(sickpeople[0].contents[0]))
+
+# 대전광역시 환자
+res = requests.get('https://www.daejeon.go.kr/corona19/index.do')
+soup = BeautifulSoup(res.text, 'html.parser')
+sickpeople = soup.select('#contBox > div:nth-child(2) > div > ul > li.tab-1 > div.txt > strong')
+print("대전광역시 확진자 : "+str(sickpeople[0].contents[0])+" 명")
+
 
